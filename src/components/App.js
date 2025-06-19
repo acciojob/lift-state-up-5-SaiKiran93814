bb8e777e5c9fdb82ced3fafa57188d0953f2a5a5
SaiKiran93814 <teppali123@gmail.com>
2025-06-19 22:33:41 +0530

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,24 @@
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
+import LoginForm from "./LoginForm";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="p-4 text-center">
+      {isLoggedIn ? (
+        <h2 className="text-green-600 text-2xl">Welcome! You are logged in.</h2>
+      ) : (
+        <LoginForm isLoggedIn={isLoggedIn} onLogin={handleLogin} />
+      )}
     </div>
-  )
+  );
 }
 
 export default App
